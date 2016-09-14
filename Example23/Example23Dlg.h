114fe716e47a7bb06c4c976b5c06bf335161fabc
shahuqing <1808475791@qq.com>
2016-09-14 15:58:20 +0800

    添加项目文件。

---
+++ Example23/Example23Dlg.h
@@ -0,0 +1,34 @@
+
+// Example23Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CExample23Dlg 对话框
+class CExample23Dlg : public CDialogEx
+{
+// 构造
+public:
+	CExample23Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EXAMPLE23_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
